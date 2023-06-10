016172f042594c060a4261b0a72d2a11f4cc5250
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2023-06-10 19:53:41 +0200

    Initial commit

---
+++ includes/js/classification.js
@@ -0,0 +1,46 @@
+// Grab all the DOM elements
+const video = document.getElementById('video'); // Video element
+const videoStatus = document.getElementById('video-status'); // Element to display video status
+const loading = document.getElementById('loading'); // Element to display loading status
+
+// Create a webcam capture
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+            video.play();
+        };
+    });
+}
+
+// A function to be called when the model has been loaded
+function modelLoaded() {
+    loading.innerText = 'Model loaded!';
+    classifier.load('./model/model.json', function() {
+        loading.innerText = "Model and custom model loaded!";
+        classifier.classify(gotResults);
+    })
+}
+
+// Extract the already learned features from MobileNet
+const featureExtractor = ml5.featureExtractor('MobileNet', modelLoaded);
+// Create a new classifier using those features
+const classifier = featureExtractor.classification(video, videoReady);
+
+// A function to be called when the video is finished loading
+function videoReady() {
+    videoStatus.innerText = 'Video ready!';
+}
+
+// Show the results
+function gotResults(err, results) {
+    // Display any error
+    if (err) {
+        console.error(err);
+    }
+    if (results && results[0]) {
+        result.innerText = results[0].label;
+        confidence.innerText = results[0].confidence;
+        classifier.classify(gotResults);
+    }
+}
